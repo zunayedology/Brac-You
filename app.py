f9a3cbc72e36f5481ab3802a3650ff540a0ce9f0
Zunayed <zunayedology@gmail.com>
2024-06-16 22:25:21 +0600

    Bug fixed

--- app.py
+++ app.py
@@ -3,23 +3,27 @@ import streamlit as st
 
 def main():
     st.title("Brac You")
+    usis = "https://usis.bracu.ac.bd"
+    confirm = st.radio(f"Have you logged into [USIS]({usis})?", ("Not Yet", "Yes"))
 
-    student_id = st.text_input("Student ID", placeholder="XXXXXXXX")
+    if confirm == "Yes":
 
-    nick_name = st.text_input("Nickname", placeholder="Chicken")
+        student_id = st.text_input("Student ID", placeholder="12345678")
+        nick_name = st.text_input("Nickname", placeholder="Chicken")
 
-    url = (
-        f"https://usis.bracu.ac.bd/academia/docuJasper/index?studentId={student_id}&reportFormat=PDF&old_id_no"
-        f"={student_id}&strMessage=&scholarProgramMsg=&companyLogo=%2Fvar%2Facademia%2Fimage%2FuniversityLogo"
-        f"%2F1571986355.jpg&companyName=BRAC+University&headerTitle=GRADE+SHEET&companyAddress=66%2C"
-        f"+MOHAKHALI+C%2FA%2C+DHAKA+-+1212.&academicStanding=Satisfactory&gradeSheetBackground=%2Fbits"
-        f"%2Fusis%2Ftomcat%2Fwebapps%2Facademia%2Fimages%2FgradeSheetBackground.jpg&_format=PDF&_name="
-        f"{nick_name}&_file=student%2FrptStudentGradeSheetForStudent.jasper"
-    )
+        url = (
+            f"https://usis.bracu.ac.bd/academia/docuJasper/index?studentId={student_id}&reportFormat=PDF&old_id_no"
+            f"={student_id}&strMessage=&scholarProgramMsg=&companyLogo=%2Fvar%2Facademia%2Fimage%2FuniversityLogo"
+            f"%2F1571986355.jpg&companyName=BRAC+University&headerTitle=GRADE+SHEET&companyAddress=66%2C"
+            f"+MOHAKHALI+C%2FA%2C+DHAKA+-+1212.&academicStanding=Satisfactory&gradeSheetBackground=%2Fbits"
+            f"%2Fusis%2Ftomcat%2Fwebapps%2Facademia%2Fimages%2FgradeSheetBackground.jpg&_format=PDF&_name="
+            f"{nick_name}&_file=student%2FrptStudentGradeSheetForStudent.jasper"
+        )
 
-    if st.link_button("Download", url):
-        if not student_id and nick_name:
+        if not (student_id and nick_name):
             st.warning("Please enter proper credentials")
+        else:
+            st.link_button("Brac This", url)
 
     footer_html = f"""
     <div style='text-align: center;'>
